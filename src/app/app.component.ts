8c189ef267b04aaace2fbf9a9fd8de94e434ef30
Rohit Sanwariya <rohitsanwariya1995@gmail.com>
2023-04-25 09:38:32 +0530

    typed form and imports.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
- 
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  NonNullableFormBuilder,
+  FormControl,
+} from '@angular/forms';
+import { DemoFormType } from './iform-type';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from './shared/material/material.module';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  standalone:true,
-
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, MaterialModule, RouterModule],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  typedFormGroup!: FormGroup;
 
+  constructor(private _fb: FormBuilder) {}
+  ngOnInit(): void {
+    this.createTypedForm();
+    this.typedFormGroup.get('remarks')?.setValue('null');
+  }
+  createTypedForm() {
+    this.typedFormGroup = this._fb.group<DemoFormType>({
+      firstName: new FormControl('John Doe'),
+      age: new FormControl(),
+      remarks: new FormControl<string>('null', { nonNullable: true }),
+    });
+  }
+
+  submit(): void {
+    console.log({ form: this.typedFormGroup.value });
+  }
 }
