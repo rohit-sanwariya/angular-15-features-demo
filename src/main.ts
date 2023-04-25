a911e64bdda918ed710269f1e8f9708376c76857
Rohit Sanwariya <rohitsanwariya1995@gmail.com>
2023-04-25 09:37:29 +0530

    laxy loading with componet standalone

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,19 @@ import { AppComponent } from './app/app.component';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { importProvidersFrom } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',loadChildren:()=>import('./app/routes.root')
+  },
+
+];
 
  bootstrapApplication(AppComponent,{
   providers:[
-    importProvidersFrom(RouterModule.forRoot(routes)),
+    importProvidersFrom(RouterModule.forRoot(routes), BrowserAnimationsModule),
   ],
 
  })
